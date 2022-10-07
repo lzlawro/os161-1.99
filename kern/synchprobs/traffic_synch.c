4f0e81229e760de519a368de03b4910d23fce929
z834li <z834li@uwaterloo.ca>
2022-10-07 18:10:00 -0400

    A1 traffic first time no collisions or deadlocks

--- kern/synchprobs/traffic_synch.c
+++ kern/synchprobs/traffic_synch.c
@@ -21,13 +21,20 @@
 /*
  * replace this with declarations of any synchronization and other variables you need here
  */
-static struct semaphore *intersectionSem;
+// static struct semaphore *intersectionSem;
 
-static struct semaphore *toNorthSem;
-static struct semaphore *toSouthSem;
-static struct semaphore *toWestSem;
-static struct semaphore *toEastSem;
+// The direction that is allowed to go
+static Direction currentDirection;
 
+// Number of vehicles waiting from each direction: north, east, south, west
+// Do I need to declare this as volatile?
+static int numWaiting[4];
+// static volatile int numInIntersection[4];
+static int numInIntersection;
+
+static struct lock *mutex;
+
+static struct cv *lightOpen[4];
 
 /* 
  * The simulation driver will call this function once before starting
@@ -36,16 +43,48 @@ static struct semaphore *toEastSem;
  * You can use it to initialize synchronization and other variables.
  * 
  */
+
+void
+intersection_change_light(void)
+{
+    int i;
+    for (i = 0; i < 4; i += 1) {
+      if (numWaiting[i] > 0) {
+        currentDirection = i;
+        // kprintf("light changed to %d\n", currentDirection);
+        cv_broadcast(lightOpen[currentDirection], mutex);
+        break;
+      }
+    }
+}
+
 void
 intersection_sync_init(void)
 {
   /* replace this default implementation with your own implementation */
+  currentDirection = random() % 4;
+  // kprintf("light changed to %d\n", currentDirection);
 
-  intersectionSem = sem_create("intersectionSem",1);
-  if (intersectionSem == NULL) {
-    panic("could not create intersection semaphore");
+  numInIntersection = 0;
+  unsigned int i;
+
+  
+  for (i = 0; i < 4; i += 1) {
+    numWaiting[i] = 0;
   }
-  return;
+
+
+  mutex = lock_create("mutex");
+  if (mutex == NULL) {
+    panic("could not create mutex lock for cv");
+  }
+
+  for (i = 0; i < 4; i += 1) {
+    lightOpen[i] = cv_create("lightOpen");
+    if (lightOpen[i] == NULL) {
+      panic("could not create cv for directions");
+    }
+  }
 }
 
 /* 
@@ -59,10 +98,17 @@ void
 intersection_sync_cleanup(void)
 {
   /* replace this default implementation with your own implementation */
-  KASSERT(intersectionSem != NULL);
-  sem_destroy(intersectionSem);
-}
+  // KASSERT(intersectionSem != NULL);
+  // sem_destroy(intersectionSem);
+  KASSERT(mutex != NULL);
+  lock_destroy(mutex);
 
+  unsigned int i;
+  for (i = 0; i < 4; i += 1) {
+    KASSERT(lightOpen[i] != NULL);
+    cv_destroy(lightOpen[i]);
+  }
+}
 
 /*
  * The simulation driver will call this function each time a vehicle
@@ -83,8 +129,41 @@ intersection_before_entry(Direction origin, Direction destination)
   /* replace this default implementation with your own implementation */
   (void)origin;  /* avoid compiler complaint about unused parameter */
   (void)destination; /* avoid compiler complaint about unused parameter */
-  KASSERT(intersectionSem != NULL);
-  P(intersectionSem);
+
+  /*
+   * Default: only allows 1 vehicle to pass through the intersection at a time.
+   * Sem is created with an initial value of 1, meaning the vehicle "quota" for the intersection
+   * P(): if value > 0, decrement
+   * if value == 0, wait until value > 0 and then decrement
+   * After the P(), vehicle enters the intersection
+   */
+
+  // KASSERT(intersectionSem != NULL);
+  // P(intersectionSem);
+
+  KASSERT(mutex != NULL);
+
+  lock_acquire(mutex);
+  numWaiting[origin] += 1;
+
+  if (numInIntersection == 0) {
+    intersection_change_light();
+  }
+
+  // kprintf("%d %d introduced, %d in intersection, %d, %d, %d, %d\n", origin, destination, numInIntersection, numWaiting[0], numWaiting[1], numWaiting[2], numWaiting[3]);
+
+  // Wait until the current allowed direction is the origin
+  while (origin != currentDirection) {
+    KASSERT(lightOpen[origin] != NULL);
+    cv_wait(lightOpen[origin], mutex);
+  }
+
+  // Vehicle enters the intersection
+  numWaiting[origin] -= 1;
+  numInIntersection += 1;
+  // kprintf("%d %d entered the intersection, %d in intersection, %d, %d, %d, %d\n", origin, destination, numInIntersection, numWaiting[0], numWaiting[1], numWaiting[2], numWaiting[3]);
+
+  lock_release(mutex);
 }
 
 
@@ -105,6 +184,15 @@ intersection_after_exit(Direction origin, Direction destination)
   /* replace this default implementation with your own implementation */
   (void)origin;  /* avoid compiler complaint about unused parameter */
   (void)destination; /* avoid compiler complaint about unused parameter */
-  KASSERT(intersectionSem != NULL);
-  V(intersectionSem);
+
+  KASSERT(mutex != NULL);
+
+  lock_acquire(mutex);
+  // Vehicle leaves the intersection
+  numInIntersection -= 1;
+  // kprintf("%d %d left the intersection, %d in intersection, %d, %d, %d, %d\n", origin, destination, numInIntersection, numWaiting[0], numWaiting[1], numWaiting[2], numWaiting[3]);
+  if (numInIntersection == 0) {
+    intersection_change_light();
+  }
+  lock_release(mutex);
 }

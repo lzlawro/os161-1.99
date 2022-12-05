da6f12b1bd374edc0c2fedbbacbe9551e4aef827
z834li <z834li@uwaterloo.ca>
2022-12-05 01:07:22 -0500

    A3: modified kill_curthread

--- kern/arch/mips/vm/dumbvm.c
+++ kern/arch/mips/vm/dumbvm.c
@@ -125,8 +125,12 @@ vm_fault(int faulttype, vaddr_t faultaddress)
 
 	switch (faulttype) {
 	    case VM_FAULT_READONLY:
+		#if OPT_A3
+		return EFAULT;
+		#else
 		/* We always create pages read-write, so we can't get this */
 		panic("dumbvm: got VM_FAULT_READONLY\n");
+		#endif
 	    case VM_FAULT_READ:
 	    case VM_FAULT_WRITE:
 		break;
